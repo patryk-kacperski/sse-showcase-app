347f216cb007045b4977dc43a0e66857b04be226
Patryk Kacperski <patryk.kacperski0@gmail.com>
2025-06-28 10:28:17 +0200

    Remove WeatherForecast endpoint and related code

--- backend/Program.cs
+++ backend/Program.cs
@@ -18,26 +18,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
 app.MapGet("/numbers", async (HttpResponse response) =>
 {
     response.Headers.Add("Content-Type", "text/event-stream");
@@ -62,8 +42,3 @@ app.MapGet("/numbers", async (HttpResponse response) =>
 .WithOpenApi();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
