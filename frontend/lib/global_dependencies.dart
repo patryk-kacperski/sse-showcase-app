1a41a75c91ed540dceb74b10928c4eb4fc421713
Patryk Kacperski <patryk.kacperski0@gmail.com>
2025-07-04 08:12:15 +0200

    Refactor dependency injection: Create GlobalDependencies widget and move MultiProvider logic from main.dart to SseShowcaseApp widget

--- frontend/lib/global_dependencies.dart
+++ frontend/lib/global_dependencies.dart
@@ -1,15 +1,26 @@
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
 import 'package:sse_showcase/services/sse_service/sse_service.dart';
 
-List<Provider> createGlobalDependencies() {
-  return [
-    Provider<http.Client>(
-      create: (_) => http.Client(),
-      dispose: (_, client) => client.close(),
-    ),
-    Provider<SseService>(
-      create: (context) => SseService(context.read<http.Client>()),
-    ),
-  ];
+class GlobalDependencies extends StatelessWidget {
+  const GlobalDependencies({super.key, required this.child});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        Provider<http.Client>(
+          create: (_) => http.Client(),
+          dispose: (_, client) => client.close(),
+        ),
+        Provider<SseService>(
+          create: (context) => SseService(context.read<http.Client>()),
+        ),
+      ],
+      child: child,
+    );
+  }
 }
--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'app.dart';
-import 'global_dependencies.dart';
+import 'package:sse_showcase/app.dart';
 
 void main() {
-  runApp(
-    MultiProvider(
-      providers: createGlobalDependencies(),
-      child: const SseShowcaseApp(),
-    ),
-  );
+  runApp(const SseShowcaseApp());
 }
